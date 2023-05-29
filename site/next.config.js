3e455c2fe5c8248035c95943f6622194b3a561b8
thekrugers <thekrugers@users.noreply.github.com>
2023-05-29 14:30:12 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'TheKrugers',
+    description: 'Kasm Workspaces we use.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://thekrugers.github.io/kasm-workspaces/',
+    contactUrl: 'https://github.com/thekrugers/kasm-workspaces/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm-workspaces/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
